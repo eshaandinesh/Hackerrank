2465436622e17d9c55c19afa34ac754fe21d0cdb
eshaandinesh <147300682+eshaandinesh@users.noreply.github.com>
2024-01-26 14:00:13 +0530

    Create XML2.py

---
+++ Python/XML2.py
@@ -0,0 +1,11 @@
+
+
+maxdepth = -1
+def depth(elem, level):
+    global maxdepth
+    # your code goes here
+    if level == maxdepth:
+        maxdepth+=1 
+    for i in elem:
+        depth(i,level+1)
+
