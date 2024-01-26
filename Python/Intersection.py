71dff0736a0e621629723da2eef2a4e58fccc169
eshaandinesh <147300682+eshaandinesh@users.noreply.github.com>
2024-01-26 13:10:37 +0530

    Create Intersection.py

---
+++ Python/Intersection.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = input()
+x = set(map(int,input().split()))
+b = input()
+y = set(map(int,input().split()))
+print(len(x.intersection(y)))
