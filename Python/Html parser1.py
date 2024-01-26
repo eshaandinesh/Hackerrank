09114b9e3eda2eafb5f2f4a0b9c8faf4d1049cc7
eshaandinesh <147300682+eshaandinesh@users.noreply.github.com>
2024-01-26 13:54:14 +0530

    Create Html parser1.py

---
+++ Python/Html parser1.py
@@ -0,0 +1,21 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from html.parser import HTMLParser
+
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print ("Start :", tag)
+        for i in attrs:
+            print('->',i[0],'>',i[1])
+    def handle_endtag(self, tag):
+        print ("End   :", tag)
+    def handle_startendtag(self, tag, attrs):
+        print ("Empty :", tag)
+        for i in attrs:
+            print('->',i[0],'>',i[1])
+
+# instantiate the parser and fed it some HTML
+parser = MyHTMLParser()
+n = int(input())
+for i in range(n):
+    parser.feed(input())
